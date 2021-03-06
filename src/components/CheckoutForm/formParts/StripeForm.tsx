a0ed66ab32ea2988a155e43179ac102b4cf42bf0
PavelJunior <pavelpyshpavel@gmail.com>
2021-03-06 14:50:17 -0800

    Add address and name from form to payment method

--- src/components/CheckoutForm/formParts/StripeForm.tsx
+++ src/components/CheckoutForm/formParts/StripeForm.tsx
@@ -44,52 +44,14 @@ const StripeForm: React.FC<Props> = ({
   const stripe = useStripe();
   const elements = useElements();
 
-  const getPaymentPayment = (id: string | undefined) => {
-    const cartTotal: number = cart.reduce<number>((sum, item) => {
-      return sum + item.quantity * item.price;
-    }, 0);
-
-    const paymentPayload = {
-      payment_method_id: id,
-      amount: cartTotal,
-    };
-
-    return paymentPayload;
-  };
-
-  const getOrderPayload = (chargeId: string) => {
-    const fullAddress = [
-      form.addressLine1,
-      form.city,
-      form.state,
-      form.zip,
-      form.country,
-    ].join(', ');
-
-    const orderItems = cart.map((item) => ({
-      id: item.id,
-      name: item.name,
-      price: item.price,
-      quantity: item.quantity,
-    }));
-
-    const fullName = [form.firstName, form.lastName].join(' ');
-
-    const orderPayload = {
-      fullName,
-      orderItems,
-      fullAddress,
-      chargeId,
-    };
-
-    return orderPayload;
-  };
-
   const handleSubmit = async () => {
     try {
+      const billingDetails = getPaymentMethodBillingDetails();
+
       const {paymentMethod} = await stripe!.createPaymentMethod({
         type: 'card',
         card: elements!.getElement(CardElement)!,
+        billing_details: billingDetails,
       });
 
       const paymentPayload = getPaymentPayment(paymentMethod?.id);
@@ -106,6 +68,65 @@ const StripeForm: React.FC<Props> = ({
     } catch (er) {
       console.log(er);
     }
+  };
+
+  const getPaymentPayment = (id: string | undefined) => {
+    const cartTotal: number = cart.reduce<number>((sum, item) => {
+      return sum + item.quantity * item.price;
+    }, 0);
+
+    const paymentPayload = {
+      payment_method_id: id,
+      amount: cartTotal,
+    };
+
+    return paymentPayload;
+  };
+
+  const getOrderPayload = (chargeId: string) => {
+    const orderItems = cart.map((item) => ({
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      quantity: item.quantity,
+    }));
+
+    const fullAddress = [
+      form.addressLine1,
+      form.city,
+      form.state,
+      form.zip,
+      form.country,
+    ].join(', ');
+
+    const orderPayload = {
+      fullName: getFullName(),
+      fullAddress,
+      orderItems,
+      chargeId,
+    };
+
+    return orderPayload;
+  };
+
+  const getFullName = () => {
+    return [form.firstName, form.lastName].join(' ');
+  };
+
+  const getPaymentMethodBillingDetails = () => {
+    const billingAddress = {
+      city: form.city,
+      country: form.country,
+      line1: form.addressLine1,
+      line2: form.addressLine2,
+      postal_code: form.zip,
+      state: form.state,
+    };
+
+    return {
+      name: getFullName(),
+      address: billingAddress,
+    };
   };
 
   return (
